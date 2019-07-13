4e2158e60baa915e299c3fa388aabdf84a498bde
Francesco Sciuto <sciuto.francesco@gmail.com>
2019-07-13 20:21:20 +0200

    Agregado projecto test

---
+++ src/GNB.Business.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GNB.Business.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
